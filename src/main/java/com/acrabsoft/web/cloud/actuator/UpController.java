df0a44a649b54ab4bb63166baf72e58a2ccc1c54
dengyifei <260721762@qq.com>
2018-12-07 17:22:08 +0800

    监控

---
+++ src/main/java/com/acrabsoft/web/cloud/actuator/UpController.java
@@ -0,0 +1,22 @@
+package com.acrabsoft.web.cloud.actuator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UpController {
+	@Autowired
+	private MyHealthChecker myHealthChecker;
+	@RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+	@RequestMapping("/up")
+    public String up(@RequestParam("up") Boolean up) {
+        myHealthChecker.setUp(up);
+ 
+        return up.toString();
+    }
+}
