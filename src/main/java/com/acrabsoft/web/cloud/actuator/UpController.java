1588e6a8df876334ad83c37d0dda325e9498865b
dengyifei <260721762@qq.com>
2018-12-07 17:32:01 +0800

    车

--- src/main/java/com/acrabsoft/web/cloud/actuator/UpController.java
+++ src/main/java/com/acrabsoft/web/cloud/actuator/UpController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UpController {
+	
 	@Autowired
 	private MyHealthChecker myHealthChecker;
 	@RequestMapping("/")
